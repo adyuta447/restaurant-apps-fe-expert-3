bdcc741c85b22d10b15fe82b0c18d0303ddae6e9
unknown <yutaagra@gmail.com>
2023-07-19 21:47:18 +0700

    first commit

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-undef */
+import RestaurantsSource from "../../data/restaurants-source";
+import UrlParser from "../../routes/url-parser";
+import { createRestaurantsDetailTemplate } from "../templates/template-creator";
+import LikeButtonPresenter from "../../utils/like-button-presenter";
+import FavoriteRestaurantIDB from "../../data/favorite-restaurant-idb";
+
+const DetailRestaurants = {
+  async render() {
+    return `
+      <h2 tabindex="0" class="title_detail">Detail Restoran</h2>
+        <section class="content"></section>
+        <div id="likeButtonContainer"></div>
+      `;
+  },
+
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurant = await RestaurantsSource.detailResto(url.id);
+    const restoranContainer = document.querySelector(".content");
+    restoranContainer.classList.add("detail_page");
+    restoranContainer.innerHTML += createRestaurantsDetailTemplate(restaurant);
+
+    LikeButtonPresenter.init({
+      likeButtonContainer: document.querySelector("#likeButtonContainer"),
+      favoriteRestaurants: FavoriteRestaurantIDB,
+      restaurant: {
+        id: restaurant.id,
+        name: restaurant.name,
+        pictureId: restaurant.pictureId,
+        description: restaurant.description,
+        city: restaurant.city,
+        rating: restaurant.rating,
+      },
+    });
+  },
+};
+
+export default DetailRestaurants;
